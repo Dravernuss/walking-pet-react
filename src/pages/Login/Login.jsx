679b420fcf3ee2a305d6677ef1052a58a7fec7ba
Gabriel <gmamanican@unsa.edu.pe>
2022-01-28 21:56:01 -0500

    Avance vista 2 - Formulario

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,30 +8,49 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import { Box, Button, TextField } from "@mui/material";
 
 const Login = () => {
   return (
     <LayoutInicial>
       <LayoutForm title="Iniciar sesión">
-        <FormControl>
-          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
-            name="radio-buttons-group"
-          >
-            <FormControlLabel
-              value="paseador"
-              control={<Radio />}
-              label="Soy paseador"
-            />
-            <FormControlLabel
-              value="cliente"
-              control={<Radio />}
-              label="Soy cliente"
-            />
-          </RadioGroup>
-        </FormControl>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              // defaultValue="female"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="paseador"
+                control={<Radio />}
+                label="Soy paseador"
+              />
+              <FormControlLabel
+                value="cliente"
+                control={<Radio />}
+                label="Soy cliente"
+              />
+            </RadioGroup>
+
+            <TextField required id="email" label="Correo electrónico" />
+            <TextField required id="password" label="Contraseña" />
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "#FFFF", color: "#000" }}
+            >
+              Ingresar
+            </Button>
+          </FormControl>
+        </Box>
       </LayoutForm>
     </LayoutInicial>
   );
