3cbd283915d322f42a1bebf96310ae65d7246fc4
LorenaRojasBello <lgrojas@indracompany.com>
2022-01-30 19:57:23 -0500

    reajuste de estilos en layout form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -14,67 +14,67 @@ const Login = () => {
   return (
     <LayoutInicial>
       <LayoutForm title="Iniciar sesión">
-        <FormControl>
-          <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
-          <RadioGroup
-            row
-            style={{
-              width: "100%",
-              justifyContent: "space-between",
-            }}
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="paseador"
-            name="radio-buttons-group"
-          >
-            <FormControlLabel
-              value="paseador"
-              control={<Radio />}
-              label="Soy paseador"
-            />
-            <FormControlLabel
-              value="cliente"
-              control={<Radio />}
-              label="Soy cliente"
-            />
-          </RadioGroup>
-          <Box
-            component="div"
-            sx={{
-              "& .MuiTextField-root": { m: 1, width: "100%" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField required id="email" label="Correo electrónico" />
-            <TextField required id="password" label="Contraseña" />
-          </Box>
-          <Button
-            variant="contained"
-            style={{
-              backgroundColor: "#FFFF",
-              color: "#000",
-              width: "35%",
-              padding: "10px 0",
-              margin: "30px auto 0",
-              borderRadius: "15px",
-              fontSize: "19px",
-              fontFamily: "Roboto-bold",
-            }}
-          >
-            Ingresar
-          </Button>
-        </FormControl>
-        <div className="login__footer">
-          <div className="login__footer-options">
-            <p className="login__footer-link">
-              <Link to="/">Olvidé mi contraseña</Link>
-            </p>
-            <div>
-              ¿Aún no tienes cuenta? <Link to="/register"> Regístrate</Link>
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
+            <RadioGroup
+              row
+              style={{
+                width: "100%",
+                justifyContent: "space-between",
+              }}
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="paseador"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="paseador"
+                control={<Radio />}
+                label="Soy paseador"
+              />
+              <FormControlLabel
+                value="cliente"
+                control={<Radio />}
+                label="Soy cliente"
+              />
+            </RadioGroup>
+            <Box
+              component="div"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "100%" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField required id="email" label="Correo electrónico" />
+              <TextField required id="password" label="Contraseña" />
+            </Box>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "#FFFF",
+                color: "#000",
+                width: "35%",
+                padding: "10px 0",
+                margin: "30px auto 0",
+                borderRadius: "15px",
+                fontSize: "19px",
+                fontFamily: "Roboto-bold",
+              }}
+            >
+              Ingresar
+            </Button>
+          </FormControl>
+          <div className="layoutForm__footer">
+            <div className="layoutForm__footer-options">
+              <p className="layoutForm__footer-link">
+                <Link to="/">Olvidé mi contraseña</Link>
+              </p>
+              <div>
+                ¿Aún no tienes cuenta? <Link to="/register"> Regístrate</Link>
+              </div>
             </div>
+            <div className="layoutForm__footer-logo"></div>
           </div>
-          <div className="login__footer-logo">{/* LOGO*/}</div>
-        </div>
       </LayoutForm>
     </LayoutInicial>
   );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -11,119 +11,116 @@ import { Box, Button, TextField } from "@mui/material";
 export const Register = () => {
   return (
     <LayoutInicial>
-      <LayoutForm title="Iniciar sesión">
-        <FormControl>
-          <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
-          <RadioGroup
-            row
-            style={{
-              width: "100%",
-              justifyContent: "space-between",
-            }}
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="paseador"
-            name="radio-buttons-group"
-          >
-            <FormControlLabel
-              value="paseador"
-              control={<Radio />}
-              label="Soy paseador"
-            />
-            <FormControlLabel
-              value="cliente"
-              control={<Radio />}
-              label="Soy cliente"
-            />
-          </RadioGroup>
-          <Box
-            component="div"
-            sx={{
-              "& .MuiTextField-root": { m: 1, width: "" },
-            }}
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              flexWrap: "wrap",
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField
-              style={{ width: "45%" }}
-              required
-              id="name"
-              label="Nombre"
-            />
-            <TextField
-              style={{ width: "45%" }}
-              required
-              id="lastName"
-              label="Apellido"
-            />
-            <TextField
-              style={{ width: "100%" }}
-              required
-              id="email"
-              label="Correo electrónico"
-            />
-            <TextField
-              style={{ width: "100%" }}
-              required
-              id="password"
-              label="Contraseña"
-            />
-            <TextField
-              style={{ width: "100%" }}
-              required
-              id="confirmPassword"
-              label="Confirmar contraseña"
-            />
-            <TextField
-              style={{ width: "100%" }}
-              required
-              id="phone"
-              label="Teléfono Fijo/Móvil"
-            />
-            <TextField
-              style={{ width: "100%" }}
-              required
-              id="city"
-              label="Ciudad"
-            />
-            <TextField
-              style={{ width: "100%" }}
-              required
-              id="address"
-              label="Dirección"
-            />
-          </Box>
-          <Button
-            variant="contained"
-            style={{
-              backgroundColor: "#FFFF",
-              color: "#000",
-              width: "35%",
-              padding: "10px 0",
-              margin: "30px auto 0",
-              borderRadius: "15px",
-              fontSize: "19px",
-              fontFamily: "Roboto-bold",
-            }}
-          >
-            Registrarse
-          </Button>
-        </FormControl>
-        <div className="login__footer">
-          <div className="login__footer-options">
-            <p className="login__footer-link">
-              <Link to="/">Olvidé mi contraseña</Link>
-            </p>
-            <div>
-              ¿Aún no tienes cuenta? <Link to="/register"> Regístrate</Link>
-            </div>
+      <LayoutForm title="Registro">
+        <div className="layoutForm__scroll">
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
+            <RadioGroup
+              row
+              style={{
+                width: "100%",
+                justifyContent: "space-between",
+              }}
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="paseador"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="paseador"
+                control={<Radio />}
+                label="Soy paseador"
+              />
+              <FormControlLabel
+                value="cliente"
+                control={<Radio />}
+                label="Soy cliente"
+              />
+            </RadioGroup>
+            <Box
+              component="div"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "" },
+              }}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                style={{ width: "45%" }}
+                required
+                id="name"
+                label="Nombre"
+              />
+              <TextField
+                style={{ width: "45%" }}
+                required
+                id="lastName"
+                label="Apellido"
+              />
+              <TextField
+                style={{ width: "100%" }}
+                required
+                id="email"
+                label="Correo electrónico"
+              />
+              <TextField
+                style={{ width: "100%" }}
+                required
+                id="password"
+                label="Contraseña"
+              />
+              <TextField
+                style={{ width: "100%" }}
+                required
+                id="confirmPassword"
+                label="Confirmar contraseña"
+              />
+              <TextField
+                style={{ width: "100%" }}
+                required
+                id="phone"
+                label="Teléfono Fijo/Móvil"
+              />
+              <TextField
+                style={{ width: "100%" }}
+                required
+                id="city"
+                label="Ciudad"
+              />
+              <TextField
+                style={{ width: "100%" }}
+                required
+                id="address"
+                label="Dirección"
+              />
+            </Box>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "#FFFF",
+                color: "#000",
+                width: "35%",
+                padding: "10px 0",
+                margin: "30px auto 0",
+                borderRadius: "15px",
+                fontSize: "16px",
+                fontFamily: "Roboto-bold",
+              }}
+            >
+              Registrarse
+            </Button>
+          </FormControl>
           </div>
-          <div className="login__footer-logo">{/* LOGO*/}</div>
-        </div>
+          <div className="layoutForm__footer">
+            <div className="layoutForm__footer-center">
+                ¿Ya tienes cuenta? <Link to="/login"> Inicia Sesión</Link>
+            </div>
+            <div className="layoutForm__footer-logo"/>
+          </div>
       </LayoutForm>
     </LayoutInicial>
   );
