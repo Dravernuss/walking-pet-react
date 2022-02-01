acced0d76e660dadd56fd102f409de7374f23f31
Gabriel <gmamanican@unsa.edu.pe>
2022-02-01 16:29:19 -0500

    About you - registerWalker

--- src/pages/Register/RegisterWalker/RegisterWalker.jsx
+++ src/pages/Register/RegisterWalker/RegisterWalker.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import LayoutForm from "../../../components/LayoutForm/LayoutForm";
 import LayoutInicial from "../../../components/LayoutInicial/LayoutInicial";
+import { AboutYou } from "./components/AboutYou";
 import { VerifyIdentity } from "./components/VerifyIdentity";
 
 export const RegisterWalker = () => {
   const [title, setTitle] = useState("Verificación de Identidad");
+  const handleChange = () => {
+    setTitle("Acerca de ti");
+  };
+
   return (
     <div>
       <LayoutInicial>
         <LayoutForm title={title}>
-          <VerifyIdentity />
+          {/* <VerifyIdentity /> */}
+          <AboutYou />
         </LayoutForm>
       </LayoutInicial>
     </div>
