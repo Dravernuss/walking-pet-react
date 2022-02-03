5101b5c9a6709d3284c80557fbbe4af953db9ada
Esteban <esteban16.rodas@gmail.com>
2022-02-03 13:16:29 -0500

    Agregando vistas Admin

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import ClientProfile from "./pages/ClientProfile/ClientProfile";
 import WalkerProfile from "./pages/WalkerProfile/WalkerProfile";
 import DatesClient from "./pages/DatesClient/DatesClient";
 import DatesWalker from "./pages/DatesWalker/DatesWalker";
+import AdminLogin from "./pagesAdmin/AdminLogin/AdminLogin";
+import ReservedTours from "./pagesAdmin/ReservedTours/ReservedTours";
+import WalkerRegistration from "./pagesAdmin/WalkerRegistration/WalkerRegistration";
+import Reports from "./pagesAdmin/Reports/Reports";
 
 const App = () => {
   return (
@@ -15,11 +19,16 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/PrincipalPage" element={<PrincipalPage />} />
-          <Route path="/ClientProfile" element={<ClientProfile />} />
-          <Route path="/WalkerProfile" element={<WalkerProfile />} />
-          <Route path="/DatesClient" element={<DatesClient />} />
-          <Route path="/DatesWalker" element={<DatesWalker />} />
+          <Route path="/principalpage" element={<PrincipalPage />} />
+          <Route path="/clientprofile" element={<ClientProfile />} />
+          <Route path="/walkerprofile" element={<WalkerProfile />} />
+          <Route path="/datesclient" element={<DatesClient />} />
+          <Route path="/dateswalker" element={<DatesWalker />} />
+          {/* Rutas Administrador */}
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/reservedtours" element={<ReservedTours />} />
+          <Route path="/walkerregistration" element={<WalkerRegistration />} />
+          <Route path="/reports" element={<Reports />} />
           <Route path="*" element={<p>404</p>} />
         </Routes>
       </div>
