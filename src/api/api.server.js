f42c00c18cf6d6d0be7b82cb3ea19c45c90e16f1
Esteban <esteban16.rodas@gmail.com>
2022-03-05 11:42:53 -0500

    Vuelve a localhost para actualizaciones

--- src/api/api.server.js
+++ src/api/api.server.js
@@ -1,2 +1,3 @@
-const API_SERVER = "https://walking-pet-api.herokuapp.com";
+// const API_SERVER = "https://walking-pet-api.herokuapp.com";
+const API_SERVER = "http://localhost:5000";
 export default API_SERVER;
